bb321485f939bc296a9ce492b4fb5d0027d35768
JerryBerry12 <85509021+JerryBerry12@users.noreply.github.com>
2023-08-25 11:48:15 +0000

    Modified 1 file
    modified: madLibsGame/madLibsGame.py

--- madLibsGame/madLibsGame.py
+++ madLibsGame/madLibsGame.py
@@ -1,21 +1,21 @@
 # Mad Libs
 # Rick Roll goes here
-# Dun Dun Dun Dun Dun Dun 
+# Dun Dun Dun Dun Dun Dun
 
 while True:
-    noun = input('Enter a noun: ')
-    verb = input('Enter a verb: ')
-    adjective = input('Enter an adjective: ')
-    noun2 = input('Enter a noun: ')
-    noun3 = input('Enter another noun: ')
-    
-    sentence = noun + ' ' + verb + ' through the ' + noun2 + ', hoping to escape the ' + adjective + ' ' + noun3 + '.'
-    print()
-    print(sentence)
-    print()
+  noun = input('Enter a noun: ')
+  verb = input('Enter a verb: ')
+  adjective = input('Enter an adjective: ')
+  noun2 = input('Enter a noun: ')
+  noun3 = input('Enter another noun: ')
 
-    answer = input('Type "q" to quit, or anything else to continue: ')
-    if answer == 'q':
-        break
+  sentence = noun + ' ' + verb + ' through the ' + noun2 + ', hoping to escape the ' + adjective + ' ' + noun3 + '.'
+  print()
+  print(sentence)
+  print()
+
+  answer = input('Type "q" to quit, or anything else to continue: ')
+  if answer == 'q':
+    break
 
 print('Bye')

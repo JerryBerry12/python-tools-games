7f927d893565b25b4bff165169959464332799c5
JerryBerry12 <85509021+JerryBerry12@users.noreply.github.com>
2023-09-04 12:52:39 +0000

    Modified 1 file
    modified: knockKnockJokes/knockKnockJokes.py
    
    Changes:
    Created an exit option so people don't have to Ctrl + C to exit.

--- knockKnockJokes/knockKnockJokes.py
+++ knockKnockJokes/knockKnockJokes.py
@@ -1,8 +1,12 @@
 print("Welcome to knock-knock jokes.")
 print("Here you will share knock-knock jokes with me.")
+print('To exit, enter the keyword "exit" when I ask you who is there.')
 print("Well, what are you waiting for? Let's begin!")
 while True:
   print("Knock-knock")
   answer1 = input("Who's there?")
+  if answer1 == "exit":
+    print('thanks for playing!')
+    exit()
   answer2 = input("Well then, " + answer1 + " who?")
   print("Ha-ha-ha")

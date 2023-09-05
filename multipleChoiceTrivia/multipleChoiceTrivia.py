6670adb5fc72b15b5e2ecaf782aac263c0a4a0be
JerryBerry12 <85509021+JerryBerry12@users.noreply.github.com>
2023-09-05 12:03:53 +0000

    Modified 1 file
    modified: multipleChoiceTrivia/multipleChoiceTrivia.py
    
    Started patching up multipleChoiceTrivia

--- multipleChoiceTrivia/multipleChoiceTrivia.py
+++ multipleChoiceTrivia/multipleChoiceTrivia.py
@@ -1,27 +1,55 @@
 # Multiple Choice Trivia
 # never gonna
 
-
 from pytrivia import Category, Diffculty, Type, Trivia
+import random
+
 my_api = Trivia(True)
 print('Welcome!')
-print('To play my multiple choice trivia game, you have to enter your guess in the blank.')
+print(
+  'To play my multiple choice trivia game, you have to enter your guess in the blank.'
+)
 while True:
-    response = my_api.request(1)
-    inner_dict = response['results'][0]
-    list_of_answers = [inner_dict['incorrect_answers']]
-    print('Question:')
-    print(inner_dict['question'])
-    print()
-    print('Options:')
-    print('a: ' + inner_dict['incorrect_answers'])
-    answer = input('Your Answer (Case Sensitive): ')
-    if answer == inner_dict['correct_answer']:
-        print('You got it!')
-    else:
-        print('No, sorry. The correct answer was ' + inner_dict['correct_answer'])
-    user_option = input('\n Press enter/return for another question. Press q + enter/return to exit.')
-    if user_option == 'q':
-        break
+  response = []
+  inner_dict = []
+  list_of_answers = []
+  response = my_api.request(1)
+  inner_dict = response['results'][0]
+  list_of_answers = [
+    inner_dict['incorrect_answers'][0], inner_dict['incorrect_answers'][1],
+    inner_dict['incorrect_answers'][2], inner_dict['correct_answer']
+  ]
+  random.shuffle(list_of_answers)
+  print('Question:')
+  print(inner_dict['question'])
+  print()
+  print('Options:')
+  print('a: ' + list_of_answers[0])
+  print('b: ' + list_of_answers[1])
+  print('c: ' + list_of_answers[2])
+  print('d: ' + list_of_answers[3])
+  option_a = list_of_answers[0]
+  option_b = list_of_answers[1]
+  option_c = list_of_answers[2]
+  option_d = list_of_answers[3]
+  answer = input('Your Answer (Case Sensitive): ')
+  if option_a == inner_dict['correct_answer']:
+    letter_of_correct_answer = option_a
+  elif option_b == inner_dict['correct_answer']:
+    letter_of_correct_answer = option_b
+  elif option_c == inner_dict['correct_answer']:
+    letter_of_correct_answer = option_c
+  elif option_d == inner_dict['correct_answer']:
+    letter_of_correct_answer = option_d
+  if answer == letter_of_correct_answer:
+    print('Horray! You got it right!')
+  else:
+    print("Sorry, you're incorrect. The correct answer is: " +
+          inner_dict['correct_answer'])
+  user_option = input(
+    '\n Press enter/return for another question. Press q + enter/return to exit.'
+  )
+  if user_option == 'q':
+    break
 
 print('Thanks for playing!')
